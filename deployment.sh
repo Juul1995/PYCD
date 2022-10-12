a53532947a6bf82f99e5f0d2eea9c38a0d1a07b1
Julia Constant <julia.constant@live.nl>
2022-10-12 19:55:01 +0200

    12-12

--- deployment.sh
+++ deployment.sh
@@ -1,29 +1,3 @@
 #!/bin/sh
 
-#https://board.wincacademy.nl/unit/view/id:5738
-
-# scp -r pycd root@178.62.229.117:/home/
-
-# apt install python3-pip
-# pip install gunicorn --upgrade
-# pip install flask 
-
-# gunicorn main:app
-# sudo systemctl restart gunicorn
-
 sudo service ssh restart
-
-zsh % print "hello world"
-
-# # install nginx 
-# sudo apt install nginx
-# sudo systemctl enable nginx
-# sudo systemctl start nginx
-
-
-
-
-#ssh root@178.62.229.117
-#systemctl enable --now pycd
-#sudo journalctl -u pycd
-
