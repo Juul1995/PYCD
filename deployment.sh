09707a3d05b042879829068f4455dd416869a5eb
Julia Constant <julia.constant@live.nl>
2022-10-15 01:23:35 +0200

    16-8

--- deployment.sh
+++ deployment.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-sudo systemctl enable nginx
-sudo systemctl start nginx
-gunicorn main:app -b 0.0.0.0
-systemctl enable --now pycd 
-
 sudo service ssh restart
 
 echo "It should have worked"
